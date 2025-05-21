48c0ec43d382c89c271f5a847e24c940b02c0326
John Owens <jdowens@google.com>
2025-05-21 10:30:52 -0700

    use a 64b build

--- dawn/build.bat
+++ dawn/build.bat
@@ -1,4 +1,4 @@
-"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
+"c:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 # set PATH=c:\Users\jdowens\Documents\working\dawn-latest\install\RelWithDebInfo\bin;%PATH%
 cmake -S . -B out/RelWithDebInfo -GNinja -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_PREFIX_PATH=c:/Users/jdowens/Documents/working/dawn-latest/install/RelWithDebInfo
 cmake --build out/RelWithDebInfo
